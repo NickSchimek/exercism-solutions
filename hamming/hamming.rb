26a0a10fa39b7e98a8c951acfad95212562f4469
Nick Schimek <cfme00@gmail.com>
2017-08-16 18:38:09 -0700

    Improve Hamming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,11 +1,7 @@
 class Hamming
   def self.compute strand1, strand2
     raise ArgumentError, "Inconsistent lengths" if strand1.length != strand2.length
-    hamming_distance = pair_up(strand1, strand2).count { |pair| pair.first != pair.last }
-  end
-
-  def self.pair_up strand1, strand2
-    strand1.chars.zip strand2.chars
+    strand1.chars.zip(strand2.chars).count { |pair| pair.first != pair.last }
   end
 end
 
